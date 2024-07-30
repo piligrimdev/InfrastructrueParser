3c4ce5955ee0757f91bafa5c96a5ee0edf730cb4
Павел <78384513+piligrimdev@users.noreply.github.com>
2024-07-30 17:04:11 +0300

    Started refactoring

--- drawio_parse.py
+++ drawio_parse.py
@@ -4,6 +4,67 @@ import html
 import unicodedata
 import json
 
+
+class DrawIOParser:
+    def __init__(self, bs_obj: BS, figures_regocnition_template: dict):
+        self.bs = bs_obj
+        self.template = figures_regocnition_template
+        self.figures_text = dict()
+        self.result = dict()
+        for key in self.template['figures_recognition'].keys():
+            self.figures_text[key] = []
+            self.result[key] = []
+
+
+
+    def recognize_figures(self) -> dict:
+        figures = bs.find_all('mxCell')
+        #retrive style parameters
+        for figure in figures:
+            if 'style' in figure.attrs.keys():
+                attrs = figure.attrs['style'].split(';')
+                style_params = dict()
+                for attr in attrs:
+                    if '=' in attr:
+                        key_value_pair = attr.split('=')
+                        style_params[key_value_pair[0]] = key_value_pair[1]
+                    else:
+                        style_params[attr] = ''
+
+                #Map figures to json entries
+                for mapping_param in self.template['figures_recognition'].items():
+                    for style_param in style_params.keys():
+                        #if style parameter and it's value equals to json template
+                        if (mapping_param[1]['param'] == style_param
+                                and mapping_param[1]['value'] == style_params[style_param]):
+                            #delete formatting, html escape chars and unicode chars from figure text
+                            cooked_data = figure.attrs['value'].replace('<div>', '').replace('</div>', '')
+                            cooked_data = html.unescape(cooked_data)
+                            cooked_data = unicodedata.normalize('NFKD', cooked_data)
+                            self.figures_text[mapping_param[0]].append(cooked_data)
+        return self.figures_text
+
+    def store_figure_values(self) -> dict:
+        self.result = dict()
+        for key in self.figures_text.keys():
+            for figure_text in self.figures_text[key]:
+                pairs = figure_text.split(';')
+                data = dict()
+
+                for pair in pairs:
+                    splited = [x.strip() for x in pair.split(':')]
+
+                    #special case for [] list of server ids for hardware figure
+                    if splited[1].startswith('['):
+                        unbracketed = splited[1].replace('[', '').replace(']', '')
+                        id_list = [int(x) for x in unbracketed.split(',')]
+                        splited[1] = [{'id': _id} for _id in id_list]
+                    data[splited[0]] = splited[1]
+
+                self.result[key].append(data)
+        return self.result
+
+
 file_path = 'drawio.xml'
 json_template = 'templates/segment_template.json'
 
@@ -18,60 +79,7 @@ result = {
 
 with open(file_path, 'r', encoding='utf-8') as file:
     bs = BS(file, 'xml')
-    #TODO read from file
-    template = {
-        'hardware': {'param': 'rounded', 'value': '0'},
-        'controllers': {'param': 'rounded', 'value': '1'},
-        'external_media': {'param': 'shape', 'value': 'cylinder3'},
-        'shd': {'param': 'rhombus', 'value': ''},
-        'telecom': {'param': 'shape', 'value': 'hexagon'},
-        'virtual_segment': {'param': 'shape', 'value': 'cloud'}
-    }
-
-    # TODO read from file
-    records = {
-        'hardware': [],
-        'controllers': [],
-        'external_media': [],
-        'shd': [],
-        'telecom': [],
-        'virtual_segment': []
-    }
-
-    items = bs.find_all('mxCell')
-    for item in items:
-        if 'style' in item.attrs.keys():
-            temp = item.attrs['style'].split(';')
-            style_params = dict()
-            for i in temp:
-                if '=' in i:
-                    nigga = i.split('=')
-                    style_params[nigga[0]] = nigga[1]
-                else:
-                    style_params[i] = ''
-
-            for map_param in template.items():
-                for param in style_params.keys():
-                    if map_param[1]['param'] == param and map_param[1]['value'] == style_params[param]:
-                        cooked_data = item.attrs['value'].replace('<div>', '').replace('</div>', '')
-                        cooked_data = html.unescape(cooked_data)
-                        cooked_data = unicodedata.normalize('NFKD', cooked_data)
-                        records[map_param[0]] += [cooked_data]
-
-    for key in records.keys():
-        for record in records[key]:
-            pairs = record.split(';')
-            data = dict()
-            for pair in pairs:
-                splited = [x.strip() for x in pair.split(':')]
-
-                if splited[1].startswith('['):
-                    temp = splited[1].replace('[', '').replace(']', '')
-                    temp_list = [int(x) for x in temp.split(',')]
-                    splited[1] = [{'id': _id} for _id in temp_list]
-
-                data[splited[0]] = splited[1]
-            result[key] += [data]
+    #TODO read template from file
 
 json_result = {}
 
